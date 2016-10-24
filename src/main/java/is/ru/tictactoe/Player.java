9d3c142fc2801ce9c3bba95383d213bb6d1c8150
Sigurður Björnsson <diddi@fivedegrees.nl>
2016-10-24 23:32:52 +0000

    Add to Player Class

--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -2,11 +2,24 @@ package is.ru.tictactoe;
 
 public class Player {
 	
-	private string name;
+	public String name;
 	private int wins = 0;
 	private int losses = 0;
 	private int draws = 0;
 	
-	public Player(string n) {
+	public Player(){}
+	
+	public Player(String n) {
 		this.name = n;
 	}
+	
+	public String getName(){
+		return Player.name;
+	}
+	
+	public bool setName(String name){
+		Player.name = name;
+		return true;
+	}
+	
+}
--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -9,7 +9,26 @@ import is.ru.tictactoe.Player;
 public class PlayerTest {
 
 	private Player player = new Player();
+	private Player playerName = new Player("Player");
 
 	@Test
+	//test empty constucter
 	public final void testPlayer() {
+		System.out.println("Check name of playerName"); 
+		Assert.assertEquals("Player", playerName.name());
 	}
+	
+	@Test
+	//test add name to Player
+	public final void testsetName(){
+		System.out.println("Set name of Player");
+		Assert.assertEquals(true, player.setName("Player2"));
+	}
+	
+	@Test
+	// get the name of the player
+	public final void testsetName(){
+		System.out.println("Get the name of Player");
+		Assert.assertEquals("Player2", player.getName());
+	
+}
