a35d4596f5405582454cc2143205e7cc28ed8f7b
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-10-26 22:52:13 +0000

    Refactor engine specific logic into engine
    Board is now an entity class with less responsibilities

--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -4,130 +4,25 @@ import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-import is.ru.tictactoe.Board;
-
 public class BoardTest {
-
-	private Board board = new Board();
-
+    
 	@Test
-	public final void testBoard() {
+	public final void testBoardSize() {
+        Assert.assertEquals(1, (new Board(1)).boardSize());
+        Assert.assertEquals(3, (new Board(3)).boardSize());
+        Assert.assertEquals(5, (new Board(5)).boardSize());
+        Assert.assertEquals(13, (new Board(13)).boardSize());
 	}
 
 	@Test
-	public final void testMakeMove() {
-		System.out.println("Inside makeMove()"); 
-
-		System.out.println("Make valid move (empty cell)"); 
-		Assert.assertEquals(true, board.makeMove(0, 0, 1));
-		
-		System.out.println("Make invalid move (occupied cell)");
-		Assert.assertEquals(false, board.makeMove(0, 0, 1));
-
-		System.out.println("Make invalid move (x=3)");
-		try {
-			board.makeMove(3,0,0);
-			fail();
-		} catch(IndexOutOfBoundsException ex) {
-			assertEquals("Illegal move", ex.getMessage());
-		}
-
-                System.out.println("Make invalid move (x=-1)");
-                try {
-                        board.makeMove(-1,0,0);
-                        fail();
-                } catch(IndexOutOfBoundsException ex) {
-                        assertEquals("Illegal move", ex.getMessage());
-                }
-
-                System.out.println("Make invalid move (y=3)");
-                try {
-                        board.makeMove(0,3,0);
-                        fail();
-                } catch(IndexOutOfBoundsException ex) {
-                        assertEquals("Illegal move", ex.getMessage());
-                }
-
-                System.out.println("Make invalid move (y=-1)");
-                try {
-                        board.makeMove(0,-1,0);
-                        fail();
-                } catch(IndexOutOfBoundsException ex) {
-                        assertEquals("Illegal move", ex.getMessage());
-                }
-
-
-		System.out.println("Make invalid move (illegal player)");
-		try {
-			board.makeMove(2,2,3);
-			fail();
-		} catch(IllegalArgumentException ex) {
-			assertEquals("Illegal player", ex.getMessage());
-		}
-
-                System.out.println("Make invalid move (neg illegal player)");
-                try {
-                        board.makeMove(2,2,-1);
-                        fail();
-                } catch(IllegalArgumentException ex) {
-                        assertEquals("Illegal player", ex.getMessage());
-                }
-
+	public final void testThatBoardIsInitiallyEmpty() {
+        Board b = new Board(3);
+        
+        for(int y = 0; y < b.boardSize(); y++) {
+            for(int x = 0; x < b.boardSize(); x++) {
+                assertEquals(-1, b.getCell(x, y));
+            }
+        }
 	}
-
-	@Test
-	public final void testGetCell() {
-		System.out.println("Inside getCell()");
-		assertEquals(0, board.getCell(0,0));
-		assertEquals(0, board.getCell(2,2));
-	}
-
-	@Test
-	public final void testWinner() {
-		System.out.println("Inside Winner()");
-		assertEquals(Board.GameResult.GAME_IN_PROGRESS, board.winner());
-
-		board.reset();
-		assertEquals(true, board.makeMove(0, 0, 1));
-		assertEquals(true, board.makeMove(1, 0, 1));
-		assertEquals(true, board.makeMove(2, 0, 1));
-		assertEquals(Board.GameResult.PLAYER_1, board.winner());
-
-                board.reset();
-                assertEquals(true, board.makeMove(0, 0, 2));
-                assertEquals(true, board.makeMove(0, 1, 2));
-                assertEquals(true, board.makeMove(0, 2, 2));
-                assertEquals(Board.GameResult.PLAYER_2, board.winner());
-
-                board.reset();
-                assertEquals(true, board.makeMove(0, 2, 2));
-                assertEquals(true, board.makeMove(1, 1, 2));
-                assertEquals(true, board.makeMove(2, 0, 2));
-                assertEquals(Board.GameResult.PLAYER_2, board.winner());
-
-                board.reset();
-                assertEquals(true, board.makeMove(2, 0, 1));
-                assertEquals(true, board.makeMove(1, 1, 1));
-                assertEquals(true, board.makeMove(0, 2, 1));
-                assertEquals(Board.GameResult.PLAYER_1, board.winner());
-
-                board.reset();
-                assertEquals(true, board.makeMove(0, 0, 1));
-                assertEquals(true, board.makeMove(0, 1, 1));
-                assertEquals(true, board.makeMove(0, 2, 2));
-
-                assertEquals(true, board.makeMove(1, 0, 2));
-                assertEquals(true, board.makeMove(1, 1, 2));
-                assertEquals(true, board.makeMove(1, 2, 1));
-
-                assertEquals(true, board.makeMove(2, 0, 1));
-                assertEquals(true, board.makeMove(2, 1, 2));
-                assertEquals(true, board.makeMove(2, 2, 1));
-
-                assertEquals(Board.GameResult.STALE_MATE, board.winner());
-
-
-	}
-
 }
 
---
+++ src/test/java/is/ru/tictactoe/EngineTest.java
@@ -0,0 +1,123 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EngineTest {
+
+	private Engine engine = new Engine(3);
+
+	@Test
+	public final void testMakeMove() throws IllegalMoveException {
+		System.out.println("Inside makeMove()"); 
+
+		System.out.println("Make valid move (empty cell)"); 
+		engine.makeMove(0, 0, 1);
+		
+		System.out.println("Make invalid move (occupied cell)");
+
+        try {
+            engine.makeMove(0, 0, 1);
+            fail();
+        } catch(IllegalMoveException ex) {
+        }
+
+		System.out.println("Make invalid move (x=3)");
+		try {
+			engine.makeMove(3,0,1);
+			fail();
+		} catch(IllegalMoveException ex) {
+			assertEquals("move is out of bounds", ex.getMessage());
+		}
+        
+        System.out.println("Make invalid move (x=-1)");
+        try {
+            engine.makeMove(-1,0,1);
+            fail();
+        } catch(IllegalMoveException ex) {
+			assertEquals("move is out of bounds", ex.getMessage());
+		}
+        
+        System.out.println("Make invalid move (y=3)");
+        try {
+            engine.makeMove(0,3,1);
+            fail();
+        } catch(IllegalMoveException ex) {
+			assertEquals("move is out of bounds", ex.getMessage());
+		}
+        
+        System.out.println("Make invalid move (y=-1)");
+        try {
+            engine.makeMove(0,-1,1);
+            fail();
+        } catch(IllegalMoveException ex) {
+			assertEquals("move is out of bounds", ex.getMessage());
+		}
+
+		System.out.println("Make invalid move (illegal player)");
+		try {
+			engine.makeMove(2,2,3);
+			fail();
+		} catch(IllegalArgumentException ex) {
+			assertEquals("Illegal player", ex.getMessage());
+		}
+
+        System.out.println("Make invalid move (neg illegal player)");
+        try {
+            engine.makeMove(2,2,-1);
+            fail();
+        } catch(IllegalArgumentException ex) {
+            assertEquals("Illegal player", ex.getMessage());
+        }
+        
+	}
+
+	@Test
+	public final void testWinner() throws IllegalMoveException {
+        Engine e = new Engine(3);
+		System.out.println("Inside Winner()");
+		assertEquals(Engine.GameResult.GAME_IN_PROGRESS, e.winner());
+        
+        e = new Engine(3);
+		e.makeMove(0, 0, 1);
+		e.makeMove(1, 0, 1);
+		e.makeMove(2, 0, 1);
+		assertEquals(Engine.GameResult.PLAYER_1, e.winner());
+        
+        e = new Engine(3);
+        e.makeMove(0, 0, 2);
+        e.makeMove(0, 1, 2);
+        e.makeMove(0, 2, 2);
+        assertEquals(Engine.GameResult.PLAYER_2, e.winner());
+
+        e = new Engine(3);
+        e.makeMove(0, 2, 2);
+        e.makeMove(1, 1, 2);
+        e.makeMove(2, 0, 2);
+        assertEquals(Engine.GameResult.PLAYER_2, e.winner());
+        
+        e = new Engine(3);
+        e.makeMove(2, 0, 1);
+        e.makeMove(1, 1, 1);
+        e.makeMove(0, 2, 1);
+        assertEquals(Engine.GameResult.PLAYER_1, e.winner());
+
+        e = new Engine(3);
+        e.makeMove(0, 0, 1);
+        e.makeMove(0, 1, 1);
+        e.makeMove(0, 2, 2);
+
+        e.makeMove(1, 0, 2);
+        e.makeMove(1, 1, 2);
+        e.makeMove(1, 2, 1);
+
+        e.makeMove(2, 0, 1);
+        e.makeMove(2, 1, 2);
+        e.makeMove(2, 2, 1);
+
+        assertEquals(Engine.GameResult.STALE_MATE, e.winner());
+	}
+
+}
+
