29dc3179a5afadb9329bad64a25fe5cb9bf22d63
rebekka123 <rebekkagudmunds@gmail.com>
2016-10-26 03:52:18 +0200

    function makeMove added to HumanPlayer and tested

--- src/main/java/is/ru/tictactoe/HumanPlayer.java
+++ src/main/java/is/ru/tictactoe/HumanPlayer.java
@@ -2,10 +2,14 @@ package is.ru.tictactoe;
 
 public class HumanPlayer extends Player{
  
- public HumanPlayer(){}
+ 	public HumanPlayer(){}
  
- public HumanPlayer(String n) {
-  this.name = n;
- }
+ 	public HumanPlayer(String n) {
+  		this.name = n;
+ 	}
+ 
+	public static boolean makeMove(int cellX, int cellY, Board gameboard, int player){
+		return gameboard.makeMove(cellX, cellY, player);
+	}
  
 }
