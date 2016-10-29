3b7473ac83643a961f159ba412f273af3a3fe129
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-10-29 11:34:53 +0000

    Remove unused imports

--- src/main/java/is/ru/tictactoe/webui/WebUI.java
+++ src/main/java/is/ru/tictactoe/webui/WebUI.java
@@ -1,13 +1,5 @@
 package is.ru.tictactoe.webui;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
