6b3370f92da0c8fcbb4a49e611a6da6b2b3c951b
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-10-30 20:31:20 +0000

    Fix formatting

--- src/main/java/is/ru/tictactoe/webui/WebUI.java
+++ src/main/java/is/ru/tictactoe/webui/WebUI.java
@@ -27,8 +27,8 @@ public class WebUI {
         // Serve static files from web directory
         staticFileLocation("/static");
         setupRoutes();
-    
-        // Debug option to see routes at  /debug/routeoverview/
+
+        // Debug option to see routes at /debug/routeoverview/
         RouteOverview.enableRouteOverview();
 
         player1 = new Player("");
@@ -51,7 +51,7 @@ public class WebUI {
             player1 = new Player(request.queryParams("player1"));
             player2 = new Player(request.queryParams("player2"));
             if (type.equals("pvc")) {
-            	player2.setHuman(false);
+                player2.setHuman(false);
             }
         }
         response.redirect("/");
@@ -61,37 +61,37 @@ public class WebUI {
     public static ModelAndView rootGetHandler(Request request, Response response) {
         // First; see if a user has entered his name
         String type = request.session().attribute(GAMETYPE);
-        if(type == null) {
+        if (type == null) {
             return new ModelAndView(null, "name_form.ftl");
         }
-        
+
         Game game = request.session().attribute("game");
-        if(game == null) {
+        if (game == null) {
             game = new Game();
             request.session().attribute("game", game);
         }
-        
+
         Map<String, Object> templateParams = new HashMap<>();
-        switch(game.winner()) {
+        switch (game.winner()) {
         case PLAYER_1:
             // Assume that player 1 is human
-            templateParams.put("message", "CONGRATULATIONS! "+ player1.getName() + " YOU WON!");
+            templateParams.put("message", "CONGRATULATIONS! " + player1.getName() + " YOU WON!");
             return new ModelAndView(templateParams, "game_results.ftl");
         case PLAYER_2:
-        	if (player2.isHuman()) {
-        		templateParams.put("message", "CONGRATULATIONS! "+ player2.getName() + " YOU WON!");
-        		return new ModelAndView(templateParams, "game_results.ftl");
-        	} else {
-        		templateParams.put("message", "SORRY "+ player1.getName() + " YOU LOST!");
-        		return new ModelAndView(templateParams, "game_results.ftl");
-        	}
+            if (player2.isHuman()) {
+                templateParams.put("message", "CONGRATULATIONS! " + player2.getName() + " YOU WON!");
+                return new ModelAndView(templateParams, "game_results.ftl");
+            } else {
+                templateParams.put("message", "SORRY " + player1.getName() + " YOU LOST!");
+                return new ModelAndView(templateParams, "game_results.ftl");
+            }
         case STALE_MATE:
             templateParams.put("message", "Close - but no cigar; this was a tie");
             return new ModelAndView(templateParams, "game_results.ftl");
         case GAME_IN_PROGRESS:
             break;
         }
-        
+
         // Populate the board template
         templateParams.put("board", game.getBoard());
 
@@ -99,28 +99,28 @@ public class WebUI {
         // If computer turn - make random move
         String playerName = "";
         if (game.getMoves() % 2 == 0) {
-        	playerName = player1.getName();
+            playerName = player1.getName();
         } else {
-        	if (!player2.isHuman()) {
-        		Random ran = new Random();
-        		// Endless loop - will break out on successful move
-        		do {
-            		int cellNum = ran.nextInt(10);
+            if (!player2.isHuman()) {
+                Random ran = new Random();
+                // Endless loop - will break out on successful move
+                do {
+                    int cellNum = ran.nextInt(10);
                     int y = cellNum / game.getBoard().boardSize();
                     int x = cellNum % game.getBoard().boardSize();
-                	try {
-						game.makeMove(x, y, 2);
-			            response.redirect("/");
-			            return null;
-					} catch (IllegalMoveException e) {
-						// do nothing - try again
-					}
-        		} while (true);
-        	} else {
-            	playerName = player2.getName();        		
-        	}
+                    try {
+                        game.makeMove(x, y, 2);
+                        response.redirect("/");
+                        return null;
+                    } catch (IllegalMoveException e) {
+                        // do nothing - try again
+                    }
+                } while (true);
+            } else {
+                playerName = player2.getName();
+            }
         }
-        
+
         templateParams.put("playerName", playerName);
         return new ModelAndView(templateParams, "board.ftl");
     }
@@ -128,15 +128,15 @@ public class WebUI {
     public static Object moveSubmissionHandler(Request request, Response response) {
         // Handle post; a player played a cell
         Game game = request.session().attribute("game");
-        if(game == null) {
+        if (game == null) {
             // Player is posting without a game session.
             // Have him sign up.
             response.redirect("/");
             return null;
         }
-        
+
         // Find the cell played
-        if(request.queryParams().size() != 1) {
+        if (request.queryParams().size() != 1) {
             halt(400, "Illegal request");
         }
 
@@ -144,22 +144,22 @@ public class WebUI {
         int cellNum = Integer.parseInt(request.queryParams().iterator().next());
         int y = cellNum / game.getBoard().boardSize();
         int x = cellNum % game.getBoard().boardSize();
-        
+
         // Make the move
         try {
             if (game.getMoves() % 2 == 0) {
-            	game.makeMove(x, y, 1);
+                game.makeMove(x, y, 1);
             } else {
-            	game.makeMove(x, y, 2);
+                game.makeMove(x, y, 2);
             }
-        } catch(IllegalMoveException e) {
+        } catch (IllegalMoveException e) {
             halt(400, "Out of bounds move");
         }
-        
+
         response.redirect("/");
         return null;
     }
-    
+
     public static Object resetGameHandler(Request request, Response response) {
         request.session().removeAttribute("game");
         response.redirect("/");
