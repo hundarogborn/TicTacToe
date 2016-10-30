c23ec57b16eb4df7d5673b7f5d9a3f90cef56106
Eiríkur Arnar <eirikurab14@ru.is>
2016-10-30 22:44:57 +0000

    Took out printTests

--- src/test/java/is/ru/tictactoe/CliTest.java
+++ src/test/java/is/ru/tictactoe/CliTest.java
@@ -9,22 +9,6 @@ public class CliTest {
 	private Cli cli = new Cli ();
 	
 	@Test
-	public final void testPrint() {
-		String string = "Testing testing, 1, 2, 3";
-		
-		System.out.printIn("Testing print function");
-		Assert.assertEquals("Testing testing, 1, 2, 3", cli.print(string));
-	}
-	
-	@Test
-	public final void testPrintIn() {
-		String string = "Testing testing, 1, 2, 3";
-		
-		System.out.printIn("Testing printin function");
-		Assert.assertEquals("Testing testing, 1, 2, 3", cli.printIn(string));
-	}
-	
-	@Test
 	public final void testGetCellCords() {
 		int index = 1;
 		
