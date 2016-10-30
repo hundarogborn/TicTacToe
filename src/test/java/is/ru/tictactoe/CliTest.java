559213af0bf9977d5b2aac9986f7581165bd76de
Sturla Halldórsson <sturlinn@gmail.com>
2016-10-30 21:15:39 +0000

    commited for master pull

--- src/test/java/is/ru/tictactoe/CliTest.java
+++ src/test/java/is/ru/tictactoe/CliTest.java
@@ -5,27 +5,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CliTest {
-
-/*
-	@Test
-	public final void testPrintBoar() {
-		char[] cell = new char[9];
-	for(int index = 0; index < 9; index++) 
-		coordinates cord = getCellCords(index);
-		int token = game.getCell(cord.x, cord.y);
-
-		if(token == 1)
-			assertEquals('X', cell[index]);
-		else if(token == 2)
-			assertEquals('O', cell[index]);
-		else 
-			assertEquals(' ', cell[index]);
-
-	}
-*/
-
-
-	//Player player = new Player(3);
 	
 	@Test
 	public final void testPlayerGetsNewName() {
@@ -40,18 +19,9 @@ public class CliTest {
 		assertEquals(1, c.setCordy(3));
 	}
 	
-
 	@Test
 	public final void testXCoordinatesCalculation() {
 		Cli c = new Cli();
 		assertEquals(0, c.setCordx(3));
 	}
-
-/*
-	@Test
-	public final void testIfPlayAgainReturnsFalseIfNIsTyped() {
-		
-		assertEquals(false, Cli.playAgain.sc.next())	
-
-	}*/
 }
