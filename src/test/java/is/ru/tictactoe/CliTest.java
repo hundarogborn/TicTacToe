95da5a85bb9594dac5f41374731c39bf482dab21
Eiríkur Arnar <eirikurab14@ru.is>
2016-10-30 22:51:53 +0000

    Minor name changes

--- src/test/java/is/ru/tictactoe/CliTest.java
+++ src/test/java/is/ru/tictactoe/CliTest.java
@@ -9,29 +9,22 @@ public class CliTest {
 	private Cli cli = new Cli ();
 	
 	@Test
-	public final void testGetCellCords() {
-		int index = 1;
-		
-		System.out.printIn("Get cell cordinants for index 1");
-		Assert.assertEquals("0, 0", cli.getCellCords(index));
-	}
-	
-	@Test
 	public final void testPlayerGetsNewName() {
-		String name = "Jón Jónsson";
-		Player player = new Player(name);
-		Assert.assertEquals("Jón Jónsson", player.name);
+	    String name = "Jón Jónsson";
+	    Player player = new Player(name);
+	    System.out.printIn("Player gets a new name");
+	    Assert.assertEquals("Jón Jónsson", player.name);
 	}
 
 	@Test
 	public final void testYCoordinatesCalculation() {	
-
+        System.out.printIn("Coordinates calculations");
 		Assert.assertEquals(1, cli.cordY(3));
 	}
 	
 	@Test
 	public final void testXCoordinatesCalculation() {
-
+        System.out.printIn("Coordinates calculations");
 		Assert.assertEquals(0, cli.cordX(3));
 	}
 	
