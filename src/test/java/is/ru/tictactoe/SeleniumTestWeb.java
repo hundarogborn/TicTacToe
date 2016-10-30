c8e34842347bf664071521b91c589f84c47469b6
Sigurður Björnsson <diddi@fivedegrees.nl>
2016-10-30 17:13:09 +0000

    foudation for Selenium

---
+++ src/test/java/is/ru/tictactoe/SeleniumTestWeb.java
@@ -0,0 +1,27 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+import is.ru.tictactoe.SeleniumTestWrapper;
+  
+  public class SeleniumTestWeb extends SeleniumTestWrapper {
+      @Test
+      public void titleMatches() {
+          driver.get(baseUrl);
+          assertEquals("TicTacToe", driver.getTitle());
+      }
+  
+      @Test
+      public void assertFirstTest() {
+          driver.get(baseUrl   "/config.html");
+          /*
+          1. Fill in some name (first name, last name)
+          2. Submit form.
+          3. Assert that form notifies of success ("Name set as: ...")
+          4. Navigate to page to get specific Joke
+          5. Enter a specific joke number
+          6. Assert the name is used in the joke.
+          */
+      }
+  }
